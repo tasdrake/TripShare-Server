9af6269850e6a1369641842331e5565c43b198c8
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-08-24 13:59:52 -0600

    Seed data

--- server/seeds/0-trips.js
+++ server/seeds/0-trips.js
@@ -1,13 +1,19 @@
-exports.seed = (knex, Promise) => {
-  return knex('trips').del()
-    .then(() => {
-      return knex('trips').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    })
-    .then(() => {
-      return knex.raw("SELECT setval('trips_id_seq', (SELECT MAX(id) FROM trips))");
-    });
+exports.seed = (knex) => {
+  return knex('trips').del().then(() => {
+    return knex('trips').insert([
+      {
+        id: 1,
+        name: '',
+        completed: false,
+        image_url: 'https://cdn.pixabay.com/photo/2016/07/30/00/03/mountain-road-1556177_1280.jpg'
+      }, {
+        id: 2,
+        name: 'Eclipse Adventure',
+        completed: false,
+        image_url: 'https://cdn.pixabay.com/photo/2016/07/30/00/03/mountain-road-1556177_1280.jpg'
+      }
+    ]);
+  }).then(() => {
+    return knex.raw("SELECT setval('trips_id_seq', (SELECT MAX(id) FROM trips))");
+  });
 };
