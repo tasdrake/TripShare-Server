0c99889aa9b602f8123ebdf19f3abde8b030f00a
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-08-24 14:02:16 -0600

    Clean code

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,18 +1,18 @@
-  module.exports = {
-    development: {
-      client: 'pg',
-      connection: {
-        database: process.env.DATABASE_URL || 'trip-share'
-      }
-    },
-    production:{
-      client: 'pg',
-      connection: process.env.DATABASE_URL
-    },
-    test: {
-      client: 'pg',
-      connection: {
-        database: process.env.DATABASE_URL || 'trip-share-test'
-      }
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      database: process.env.DATABASE_URL || 'trip-share'
     }
-  };
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  },
+  test: {
+    client: 'pg',
+    connection: {
+      database: process.env.DATABASE_URL || 'trip-share-test'
+    }
+  }
+};
