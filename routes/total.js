61ef75116084c7fea60c305daba09ce4ea762f95
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-08-25 11:21:51 -0600

    added route for totaling

--- routes/total.js
+++ routes/total.js
@@ -5,7 +5,6 @@ const knex = require('../knex');
 router.get('/:id', (req, res) => {
   const id = req.params.id;
   knex('trips')
-    // .select('*')
     .where('trips.id', id)
     .join('users', 'trips.id', 'users.trip_id')
     .select([
@@ -17,7 +16,27 @@ router.get('/:id', (req, res) => {
       let total = 0;
       people.map(e => total += e.amount_spent);
       const individualCost = total / people.length;
-      people.map()
+      let individualTotal = 0; // check how close to the total
+      people.map(e => {
+        if (e.amount_spent > individualCost) {
+          // for exact cost
+          //
+          // e.amount_owed = -(e.amount_spent - individualCost).toFixed(2);
+
+          // for whole dollar costs
+          e.amount_owed = -Math.ceil(e.amount_spent - individualCost);
+          individualTotal += e.amount_owed;
+        } else {
+          // for exact cost
+          //
+          // e.amount_owed = +(individualCost - e.amount_spent).toFixed(2);
+
+          // for whole dollar costs
+          e.amount_owed = +Math.ceil(individualCost - e.amount_spent);
+          individualTotal += e.amount_owed;
+        }
+      });
+      people.unshift({ total, individualCost, individualTotal });
       res.send(people);
     })
     .catch(err => console.error(err));
