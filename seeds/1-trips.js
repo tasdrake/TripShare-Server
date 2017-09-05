b5015020d7afab03183d8b1b4c413046182e5100
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-09-05 14:17:25 -0600

    updated seeds

--- seeds/1-trips.js
+++ seeds/1-trips.js
@@ -5,13 +5,13 @@ exports.seed = (knex) => {
         id: 1,
         name: 'Coachella',
         completed: false,
-        image_url: 'https://cdn.pixabay.com/photo/2016/07/30/00/03/mountain-road-1556177_1280.jpg',
+        image_url: 'https://upload.wikimedia.org/wikipedia/commons/d/d0/Coachella_2012_weekend_2_day_2_sunset.jpg',
         admin_id: 1,
       }, {
         id: 2,
         name: 'Eclipse Adventure',
         completed: false,
-        image_url: 'https://cdn.pixabay.com/photo/2016/07/30/00/03/mountain-road-1556177_1280.jpg',
+        image_url: '../css/road.jpg',
         admin_id: 1,
       }
     ]);
@@ -19,3 +19,5 @@ exports.seed = (knex) => {
     return knex.raw("SELECT setval('trips_id_seq', (SELECT MAX(id) FROM trips))");
   });
 };
+
+// default: https://cdn.pixabay.com/photo/2016/07/30/00/03/mountain-road-1556177_1280.jpg
