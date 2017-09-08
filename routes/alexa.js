ba0c03566f151931d198384da2868d044c9ecec6
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-09-07 21:56:12 -0600

    clean code

--- routes/alexa.js
+++ routes/alexa.js
@@ -10,47 +10,23 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   const body = req.body;
-  console.log(req.body);
   body.name = body.name[0].toUpperCase() + body.name.slice(1);
   body.trip = body.trip[0].toUpperCase() + body.trip.slice(1);
-  console.log(body);
-  // 50.42
-  // res.send(req.body);
   knex('users')
     .select('*')
     .where('name', body.name)
     .then(user => {
       user[0].amount_spent = Number(req.body.amount) + Number(user[0].amount_spent);
-      console.log(user[0]);
-      console.log('----------------');
-      console.log(user[0].id);
-      console.log(user[0].name);
-      console.log(typeof user[0].amount_spent);
-
-      // knex('users')
-      //   .update(user[0].amount_spent)
-      //   .where('name', body.name)
-      //   // .where('id', user[0].id)
-      //   // .returning([
-      //   //   'name',
-      //   //   'amount_spent'
-      //   // ])
-      //   // .then(newUser => console.log(newUser))
-      //   // .catch(err => console.log(err))
-
       knex('users')
-        // .update(user[0].amount_spent)
-        // .select('*')
         .update({amount_spent: user[0].amount_spent})
         .where('id', user[0].id)
         .returning([
           'name',
           'amount_spent',
-          'amount_owed',
-          'paid',
-          'image_url'
         ])
-        .then(updatedUser => console.log(updatedUser))
+        .then(updatedUser => {
+          res.send(updatedUser);
+        })
         .catch(err => console.error(err));
     });
 });
