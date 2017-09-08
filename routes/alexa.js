b67c5bea1bfdf0e543550d0f755c68e6b1bb41a2
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-09-07 20:53:10 -0600

    alexa

--- routes/alexa.js
+++ routes/alexa.js
@@ -10,8 +10,15 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   const body = req.body;
+  body.name = body.name[0].toUpperCase() + body.name.slice(1);
+  body.trip = body.trip[0].toUpperCase() + body.trip.slice(1);
   console.log(body);
-  res.send(req.body);
+
+  // res.send(req.body);
+  knex('users')
+    .select('*')
+    .where('name', body.name)
+    .then(user => console.log(user))
 });
 
 
